e783d5f52697c70d1096fd604a38451a7834143b
JayTaravia2002 <jaytaravia@gmail.com>
2023-01-29 01:16:45 +0530

    Chat app using kotlin

---
+++ app/src/main/java/com/jaytaravia/chatappinkotlin/Message.kt
@@ -0,0 +1,15 @@
+package com.jaytaravia.chatappinkotlin
+
+class Message {
+
+    var message: String? = null
+    var senderId: String? = null
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+
+}
